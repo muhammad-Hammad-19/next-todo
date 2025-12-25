bec28cd1e435cc496992b0ad8d659a21ffb8af96
hammad <muhammad-Hammad-19@gmail.com>
2025-12-25 21:52:29 +0500

    Your descriptive message

--- src/app/provider/Provider.js
+++ src/app/provider/Provider.js
@@ -1,5 +1,5 @@
 "use client";
-import { store } from "../store/store";
+import { store } from "@/app/store/store";
 import { Provider } from "react-redux";
 export default function ReactQueryProvider({ children }) {
   return <Provider store={store}>{children}</Provider>;
--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../feature/todo/todoSilice.js"; // make sure the path is correct
+import counterReducer from "@/app/feature/todo/todoSilice.js"; // make sure the path is correct
 
 export const store = configureStore({
   reducer: {
